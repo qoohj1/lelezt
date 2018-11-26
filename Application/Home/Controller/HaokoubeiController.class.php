e6261869c320beffb1597173411c4623db6a917d
qoohj1 <qoohj@qq.com>
2018-11-26 19:07:30 +0800

    up

--- Application/Home/Controller/HaokoubeiController.class.php
+++ Application/Home/Controller/HaokoubeiController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class HaokoubeiController extends Controller {
     public function index(){
-      $data = M('test_haokoubei')->select();
+      // $data = M('test_haokoubei')->select();
       // var_dump($data);
       $this->display();
     }
