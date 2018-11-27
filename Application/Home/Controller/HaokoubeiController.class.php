1ad6cd57109627c43e51e53dd83fc0bf69a168bb
qoohj1 <qoohj@qq.com>
2018-11-27 11:43:27 +0800

    UP

--- Application/Home/Controller/HaokoubeiController.class.php
+++ Application/Home/Controller/HaokoubeiController.class.php
@@ -3,8 +3,23 @@ namespace Home\Controller;
 use Think\Controller;
 class HaokoubeiController extends Controller {
     public function index(){
-      // $data = M('test_haokoubei')->select();
-      // var_dump($data);
       $this->display();
+    }
+    // 投票
+    public function vote(){
+      // 判断ip是否已投票
+      // 获取客户端ip
+      $condition['ip'] = $_SERVER['REMOTE_ADDR'];
+      // 获取投票选项
+      $condition['id'] = I('item');
+      $server_ip = M('server_ip');
+      $res = $server_ip->where($condition)->find();
+      var_dump($res);
+      if($res){
+        jsonReturn(1,error,'每个ip只能投票一次');
+      }else{
+        // $server_ip->add
+        // jsonReturn(0,success,)
+      }
     }
 }
