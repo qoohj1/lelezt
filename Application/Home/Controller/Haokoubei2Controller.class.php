d66f65a1f2baa37f769e4985e02a35eaa662e038
qoohj1 <qoohj@qq.com>
2018-12-06 02:57:22 +0800

    up

--- Application/Home/Controller/Haokoubei2Controller.class.php
+++ Application/Home/Controller/Haokoubei2Controller.class.php
@@ -22,7 +22,7 @@ class Haokoubei2Controller extends Controller {
       $map['id'] = I('item');
       // 判断ip是否已投票
       // 获取客户端ip
-      $condition['ip'] = $_SERVER['REMOTE_ADDR'];
+      $condition['ip'] = $this->getIP();
       $data['ip'] = $condition['ip'];
       $server_ip = M('server_ip');
       $votelist = M('votelist');
@@ -42,6 +42,29 @@ class Haokoubei2Controller extends Controller {
         }
       }
     }
+
+    function getIP(){
+        static $realip;
+        if (isset($_SERVER)){
+            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
+                $realip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+            } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+                $realip = $_SERVER["HTTP_CLIENT_IP"];
+            } else {
+                $realip = $_SERVER["REMOTE_ADDR"];
+            }
+        } else {
+            if (getenv("HTTP_X_FORWARDED_FOR")){
+                $realip = getenv("HTTP_X_FORWARDED_FOR");
+            } else if (getenv("HTTP_CLIENT_IP")) {
+                $realip = getenv("HTTP_CLIENT_IP");
+            } else {
+                $realip = getenv("REMOTE_ADDR");
+            }
+        }
+        return $realip;
+    }
+
     // 抽奖
     public function lottery(){
       // $prize_arr = array(
