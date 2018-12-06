59a97aff8534bd2e00d4aa6d16b20b6fb8d80de1
qoohj1 <qoohj@qq.com>
2018-12-06 23:41:58 +0800

    up

---
+++ Application/Home/Controller/Haokoubei3Controller.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class Haokoubei3Controller extends Controller {
+    public function index(){
+      $appid='wx7f1344c32363bbf3';
+      $redirect_uri = 'http://shegurz.com';
+      $scope = 'snsapi_userinfo';//弹出授权页面 snsapi_base静默授权
+      $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=$scope&state=123#wechat_redirect";
+      redirect($url);
+    }
+}
